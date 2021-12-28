460edba6ee8b98d423dddffde24cb2c18d0d1abf
dapeng <1481090123@qq.com>
2021-12-29 00:38:17 +0800

    调整程序输出，增加注释

--- 全国计算机等级考试二级教程/C++语言程序设计/第3章 基本控制结构/编程题/10、打印图像-方法1.cpp
+++ 全国计算机等级考试二级教程/C++语言程序设计/第3章 基本控制结构/编程题/10、打印图像-方法1.cpp
@@ -3,32 +3,34 @@ using namespace std;
 
 int main()
 {
+	// 输出上三角形
 	for (int i = 1; i <= 3; i++)
 	{
 		for (int j = 1; j <= 4 - i; j++)
 		{
-			cout << " ";
+			cout << "  ";
 		}
 
 		for (int j = 1; j <= 2 * i - 1; j++)
 		{
-			cout << "*";
+			cout << "* ";
 		}
 		cout << endl;
 	}
 	
-	cout << "*******" << endl;
+	// 输出中部横线
+	cout << "* * * * * * *" << endl;
 	
-	
+	// 输出下三角形
 	for (int i = 1; i <= 3; i++)
 	{
 		for (int j = 1; j <= i; j++)
 		{
-			cout << " ";
+			cout << "  ";
 		}
 		for (int j = 1; j <= 2 * (4 - i) - 1; j++)
 		{
-			cout << "*";
+			cout << "* ";
 		}
 		cout << endl;
 	}
